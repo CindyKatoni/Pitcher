6505af364dcb5f84fb135211ed417a65e47a238f
CindyKatoni <katmuema@gmail.com>
2020-09-22 22:36:33 +0300

    Login logic and redirect to home page

--- pitch/models.py
+++ pitch/models.py
@@ -1,9 +1,12 @@
 from pitch import db, login_manager
+from flask_login import UserMixin
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 #Model classes for the database
-class User(db.Model):
+class User(db.Model, UserMixin):
     #table name
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)

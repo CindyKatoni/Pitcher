149234a1b58dec5f9bad12e6bc1abf6386e2f423
CindyKatoni <katmuema@gmail.com>
2020-09-23 15:02:53 +0300

    AdAdded pitch forms for each category

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -14,8 +14,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'#USE POSTGRE LATER ON
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager.init_app(app)####
 login_manager.login_view = 'login'
+login_manager.login_message_category = 'primary'
 
 #Import here to prevent circular imports
 from pitch import routes
--- pitch/routes.py
+++ pitch/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from pitch import app, db, bcrypt
 from pitch.forms import RegistrationForm, LoginForm
 from pitch.models import User, Pitch
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route("/")
@@ -39,7 +39,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -48,3 +49,14 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Your Accout', image_file=image_file)
+
+@app.route("/pitch/new")
+@login_required
+def new_pitch():
+    return render_template('create_pitch.html', title='New Pitch')
